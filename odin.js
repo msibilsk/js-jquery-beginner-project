b5fdfe324ab1eba3f7b9c5ccec34af96b93d9a98
Mary-El Willis <msibilsk@users.noreply.github.com>
2015-02-09 15:30:15 -0500

    Update odin.js
    adding function to addClass that changes div colors on mouseenter

--- odin.js
+++ odin.js
@@ -3,6 +3,8 @@ var height = 16;
 var width = 16;
 
 for (i=0; i<height*width; i++) {
-$('.container').append('<div></div>');
+$('.container').append('<div class="black"></div>');
 }
+    $(".black").mouseenter(function(){
+        $(this).addClass("red")});
 });
