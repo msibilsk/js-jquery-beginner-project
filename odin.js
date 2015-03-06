7781c93227f69cec957a05ab5ecd79594043dd22
Mary-El Willis <msibilsk@users.noreply.github.com>
2015-03-06 16:27:07 -0500

    Update odin.js
    adds random color feature

--- odin.js
+++ odin.js
@@ -5,7 +5,18 @@ for (i=0; i<Math.pow(w, 2); i++) {
 $('.container').append('<div class="grey"></div>');
 }
 
-$("button").click(function() {
+function getRandomColor() {
+   return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+}
+                     
+$(".grey").css("height", newDim);
+$(".grey").css("width", newDim);
+
+ $(".grey").mouseenter(function() {
+    $(this).addClass("black");  
+ });
+    
+$("#black").click(function() {
 $(".black").remove();
 $(".grey").remove();
 w = prompt("How many blocks per side?");
@@ -15,15 +26,20 @@ $('.container').append('<div class="grey"></div>');}
 $(".grey").css("height", newDim);
 $(".grey").css("width", newDim);
 $(".grey").mouseenter(function(){
-$(this).addClass("black");
-});    
+$(this).addClass("black");});
 });
 
+$("#random").click(function() {
+$(".black").remove();
+$(".grey").remove();
+w = prompt("How many blocks per side?");
+newDim = $(".container").width()/(w);
+for (i=0; i<Math.pow(w, 2); i++) {
+$('.container').append('<div class="grey"></div>');}
 $(".grey").css("height", newDim);
 $(".grey").css("width", newDim);
-    
 $(".grey").mouseenter(function(){
-$(this).addClass("black");
+    $(this).css("background-color", getRandomColor());});
 });
     
 });
